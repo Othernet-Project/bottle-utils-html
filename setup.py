948883b1729dda27c9fcb8998e7195de7aab48c8
Branko Vukelic <branko@brankovukelic.com>
2015-03-31 18:27:59 +0200

    Versioning fix

--- setup.py
+++ setup.py
@@ -3,19 +3,23 @@ from setuptools import setup, find_packages
 
 import bottle_utils
 
+
 def read(fname):
     """ Return content of specified file """
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+
 VERSION = bottle_utils.__version__
-RELEASE = '0.3'
+MAJOR = '0.3'
+NEXT = '0.4'
 
 setup(
     name='bottle-utils-html',
     version=VERSION,
     author='Outernet Inc',
     author_email='branko@outernet.is',
-    description=('HTML utilities for developing apps with Bottle web framework'),
+    description=('HTML utilities for developing apps with Bottle web '
+                 'framework'),
     license='BSD',
     keywords='bottle utils html templates',
     url='http://outernet-project.github.io/bottle-utils/',
@@ -24,7 +28,7 @@ setup(
     install_requires=[
         'bottle==0.12.7',
         'python-dateutil==2.2',
-        'bottle-utils-common~=%s' % RELEASE,
+        'bottle-utils-common >=%s, <%s' % (MAJOR, NEXT),
     ],
     classifiers=[
         'Development Status :: 3 - Alpha',
